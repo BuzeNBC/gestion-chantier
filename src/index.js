9150a0ef9696951f623c2f212c45589fdafef43e
BuzeNBC <contact@nordbaticonstruction.com>
2024-12-16 13:58:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Ajout d'un test visuel
+console.log('Script de démarrage exécuté');
+document.body.innerHTML += '<div>Test de chargement</div>';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
